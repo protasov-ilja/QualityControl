410b123b90f34fe4d20407f7e8038a103a006a87
Ilya Protasov <pfltriot@gmail.com>
2018-09-16 21:38:13 +0300

    перенес ехешник и тесты в отдельную папку

---
+++ lab1/TriangleExe/tests.bat
@@ -0,0 +1,120 @@
+REM running program without arguments
+lab1.exe
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM running program with one argument
+lab1.exe 1
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM running program with two arguments
+lab1.exe 1 2
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM running program with four arguments
+lab1.exe 1 2 3 4
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM running program without numbers on input
+lab1.exe a b c
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM running program with one no number on input
+lab1.exe 1 2 str
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM running program there first argumenyt is not a number
+lab1.exe a 2 3
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM running program where first num is less then zero 0
+lab1.exe -1 3.3 5.5
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM running program where second num is less then zero 0
+lab1.exe 1.1 -1 2.2
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM running program where third num is less then zero 0
+lab1.exe 1.1 2.2 -1
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM running program where all nums are 0
+lab1.exe 0 0 0 > tests\out.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\out.txt tests\not_triangle.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM running program where first num is 0
+lab1.exe 0 2 3 > tests\out.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\out.txt tests\not_triangle.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM running program where second num is 0
+lab1.exe 1 0 3 > tests\out.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\out.txt tests\not_triangle.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM running program where third num is 0
+lab1.exe 1 2 0 > tests\out.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\out.txt tests\not_triangle.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM running program where shape is not triangle
+lab1.exe 1 2 100 > tests\out.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\out.txt tests\not_triangle.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM running program where shape is not triangle
+lab1.exe 1 100 3 > tests\out.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\out.txt tests\not_triangle.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM running program where shape is not triangle
+lab1.exe 100 2 3 > tests\out.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\out.txt tests\not_triangle.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM running program where shape is common triangle
+lab1.exe 3.1 4.1 5.1 > tests\out.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\out.txt tests\common_triangle.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM running program where 1 and 2 sides are equale
+lab1.exe 4.1 4.1 5.1 > tests\out.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\out.txt tests\isosceles_triangle.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM running program where 2 and 3 sides are equale
+lab1.exe 4.1 4.1 5.1 > tests\out.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\out.txt tests\isosceles_triangle.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM running program where 3 and 1 sides are equale
+lab1.exe 4.1 5.1 4.1 > tests\out.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\out.txt tests\isosceles_triangle.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM running program where all sides are equale
+lab1.exe 3.1 3.1 3.1 > tests\out.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\out.txt tests\equilateral_triangle.txt
+IF ERRORLEVEL 1 GOTO err
+
+
+ECHO Program testing succeeded :-)
+
+EXIT
+
+:err
+ECHO Program testing failed :-(
+EXIT
